11e06a1cdb4c0173c4148548cb51680b17681678
Generoso Pagano <generoso.pagano@inria.fr>
2014-11-13 16:16:31 +0100

    Upload update site for framesoc.importers

---
+++ updatesite/upload-site.sh
@@ -0,0 +1,95 @@
+#!/bin/bash
+
+#####################################################################
+# Upload to the soctrace-inria website the eclipse update site for a
+# given module.
+#
+# Author: Generoso Pagano
+#####################################################################
+
+# Configuration
+
+#####################################################################
+# In the functions below we work under the hypothesis that the script
+# is in the update site directory.
+#####################################################################
+
+# script directory == update site directory
+UPDATESITE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# update site content
+SITE_CONTENT="features plugins artifacts.jar content.jar"
+
+# projects
+PROJECTS="framesoc ocelotl framesoc.importers"
+
+
+# print the help
+function help() {
+    echo ""
+    echo "Usage: ./upload-site.sh LOCAL_SITE PROJECT"
+    echo "LOCAL_SITE : absolute path of the directory containing the Eclipse update site content (${SITE_CONTENT})"
+    echo "PROJECT : one of {$PROJECTS}"
+    echo ""
+    exit
+}
+
+# verify if the string in $2 is contained in the list $1
+function contains() {
+    [[ $1 =~ $2 ]] && echo 1 || echo 0
+}
+
+# main function
+function main() {
+    
+    if [ $# != 2 ] ; then
+	echo "Error: wrong number of arguments."
+	help
+    fi
+
+    LOCAL_SITE=$1
+    PROJECT=$2
+
+    if [ ! -d "$LOCAL_SITE" ]; then
+	echo "Error: directory $LOCAL_SITE does not exist."
+	help
+    fi
+
+    KNOWN=`contains "${PROJECTS}" "${PROJECT}"`
+    if [ $KNOWN = "0" ]; then
+	echo "Error: project $PROJECT is unknown"
+	help
+    fi
+
+    echo "* Removing the old update site for project $PROJECT"
+    rm -rf $PROJECT/*
+
+    echo "* Copying the new update site content"
+    for OBJ in $SITE_CONTENT; do
+	cp -r "${LOCAL_SITE}/${OBJ}" "${PROJECT}"
+    done
+
+    echo "* Uploading modifications"
+    git add --all
+    git commit -m "Upload update site for ${PROJECT}"
+    git push
+
+    RET=$?
+    if [ "$RET" = "0" ]; then
+	echo "Tudo bem"
+    else
+	echo "An error occurred while uploading the site"
+    fi
+
+}
+
+###############
+
+OLD_DIR=`pwd`
+cd "${UPDATESITE_DIR}"
+
+main $@
+
+cd "${OLD_DIR}"
+
+##############
